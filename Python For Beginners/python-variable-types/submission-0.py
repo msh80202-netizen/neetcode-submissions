a8a363affd315182b164dc65569861842eb14480
msh80202-netizen <msh80202@gmail.com>
2026-08-27 23:50:55 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 1
+float_type = 10.13
+bool_type = False
+string_type = "birthday"
+list_type = [2, 0, 0, 2]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
