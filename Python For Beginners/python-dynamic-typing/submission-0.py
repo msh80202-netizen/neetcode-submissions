f15aad93627f0797e3ed1c0adf3af6a5cd099e55
msh80202-netizen <msh80202@gmail.com>
2026-08-27 23:51:52 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.13
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "name"
+print(type(variable))
+variable = [1, 2]
+print(type(variable))
